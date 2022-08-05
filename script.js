7872445cc2ef288be5427efc1f4279fd75b875eb
JUstplstry <rodeantavgolding@gmail.com>
2022-08-05 17:07:02 -0400

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+let phone = document.getElementById("Phone")
+
+phone.onclick = function(){
+	document.getElementById("Phone").style.fontSize = "13px"
+	document.getElementById("Phone").style.fontFamily = "Trebuchet MS"
+	phone.innerHTML = "Phone number: 347-419-4439"
+}
+
+
+let page = document.getElementById("PFP")
+page.onclick = function(){
+	document.getElementById("page").style.backgroundColor = "#E0B0FF	"
+
+	document.getElementById("about_Me_Section").style.backgroundColor = "white	"
+
+	document.getElementById("my_experience").style.backgroundColor = "white	"
+
+
+	document.getElementById("my_Contact").style.backgroundColor = "white	"
+}
+
+
